52f8b81f590c7072120ae9d10361740923b7dcd8
Manideep <17101992m@gmail.com>
2022-06-01 00:48:00 +0530

    Third recursion problem solved

---
+++ Recursion Problem 3.js
@@ -0,0 +1,17 @@
+//Q: Write a function called productOfArray which takes in an array of number and returns th eproduct of them all.
+
+
+//Got the hint from the lectures of recursion for this solution
+function productOfArray(array) {
+    let num = 1;
+
+    function pro(arr){
+        if(arr.length === 0) return;
+        num = num * arr[0];
+        return pro(arr.slice(1))
+    }
+    pro(array)
+    return num;
+}
+
+productOfArray([1,2,3])
