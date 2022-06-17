131c38d5e9fa3d0da791b9d4eaf7ec9202bcf224
manideep <17101992m@gmail.com>
2022-06-17 19:09:26 +0530

    Bubble short Q1 solved

---
+++ Bubble Sort Q1.js
@@ -0,0 +1,17 @@
+//A sorting algorithm where the largest value bubble up to the top.
+
+function bubbleSort(arr){
+    for(let i = arr.length-1; i >= 0; i--){
+        for(let j = 0; j < i; j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+    // return console.log(arr);
+}
+
+bubbleSort([3,1,9,10,6,77,43])
