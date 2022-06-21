0f199c09fa37f0d116861f760e276dd8d9880805
manideep <17101992m@gmail.com>
2022-06-21 19:12:26 +0530

    Optimised solution for bubble sort added

--- Bubble Sort Q1.js
+++ Bubble Sort Q1.js
@@ -1,5 +1,5 @@
 //A sorting algorithm where the largest value bubble up to the top.
-
+// /*
 function bubbleSort(arr){
     for(let i = arr.length; i > 0; i--){
         for(let j = 0; j < i - 1; j++){
@@ -15,7 +15,9 @@ function bubbleSort(arr){
 }
 
 bubbleSort([3,1,9,10,6,77,43])
+// */
 
+// /*
 function bubbleSort(arr){
     for(let i = arr.length; i > 0; i--){
         for(let j = 0; j < i - 1; j++){
@@ -29,3 +31,25 @@ function bubbleSort(arr){
 }
 
 bubbleSort([3,1,9,10,6,77,43])
+// */
+
+// /*
+//Optimised solution(If there is no swap it will break out the loop)
+function bubbleSort(arr){
+    let noSwap;
+    for(let i = arr.length; i > 0; i--){
+        noSwap = true;
+        for(let j = 0; j < i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]] //Swapping the element using ES6 syntax
+                noSwap = false;
+            }
+        }
+        if(noSwap) break;
+    }
+    // return arr;
+    return console.log(arr);
+}
+
+bubbleSort([3,1,9,10,6,77,43])
+// */
