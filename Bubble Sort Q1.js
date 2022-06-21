2a297cffa5445d2530df03a73e31e6895a1b81f0
manideep <17101992m@gmail.com>
2022-06-21 19:00:04 +0530

    Es6 syntax implemented for swapping the element

--- Bubble Sort Q1.js
+++ Bubble Sort Q1.js
@@ -15,3 +15,17 @@ function bubbleSort(arr){
 }
 
 bubbleSort([3,1,9,10,6,77,43])
+
+function bubbleSort(arr){
+    for(let i = arr.length; i > 0; i--){
+        for(let j = 0; j < i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]] //Swapping the element using ES6 syntax
+            }
+        }
+    }
+    return arr;
+    // return console.log(arr);
+}
+
+bubbleSort([3,1,9,10,6,77,43])
