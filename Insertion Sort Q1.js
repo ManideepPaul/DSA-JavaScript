d43e03f1751a912cf9b942a1631dea54a654ddad
manideep <17101992m@gmail.com>
2022-06-22 19:23:26 +0530

    insertion sort solved

--- Insertion Sort Q1.js
+++ Insertion Sort Q1.js
@@ -4,3 +4,16 @@
 // => Now compare the second element with the one before it and swap if necessary.
 // => Continue to the next element and if it is in the incorrect order, iterate through the sorted portion (i.e. the left side) to place the element in the correct palce.
 // => Repeat until the array is sorted.
+
+const insertionSort = (arr) => {
+    for(let i = 1; i < arr.length; i++){
+        for(let j = 0; j < i; j++){
+            if(arr[j] > arr[i]){
+                [arr[i], arr[j]] = [arr[j], arr[i]]
+            }
+        }
+    }
+    return console.log(arr)
+}
+
+insertionSort([3,8,2,12,90,34,23,])
