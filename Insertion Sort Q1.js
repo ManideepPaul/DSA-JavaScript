61fd7bf2a4616ebde948ed1e20f8d81d04fee410
manideep <17101992m@gmail.com>
2022-06-24 11:26:54 +0530

    Insertion sort solved using Colt Steele's solution

--- Insertion Sort Q1.js
+++ Insertion Sort Q1.js
@@ -5,8 +5,22 @@
 // => Continue to the next element and if it is in the incorrect order, iterate through the sorted portion (i.e. the left side) to place the element in the correct palce.
 // => Repeat until the array is sorted.
 
+function insertionSort(arr){
+    let l,p,temp;
+    for(l = 1; l < arr.length; l++){
+        temp = arr[l];
+        for(p = l - 1; p >= 0 && arr[p] > temp ; p-- ){
+            arr[p + 1] = arr[p];
+        }
+        arr[p+1] = temp;
+    }
+    return console.log(arr)
+}
+
+insertionSort([3,8,2,12,90,34,23,])
+
 //I don't know that if this is following insertion sort or not.
-const insertionSort = (arr) => {
+const insertionSort1 = (arr) => {
     for(let i = 1; i < arr.length; i++){
         for(let j = 0; j < i; j++){
             if(arr[j] > arr[i]){
@@ -17,4 +31,4 @@ const insertionSort = (arr) => {
     return console.log(arr)
 }
 
-insertionSort([3,8,2,12,90,34,23,])
+insertionSort1([3,8,2,12,90,34,23,])
