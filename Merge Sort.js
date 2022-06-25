09619158898e406a005855752b245fd9bbabc3ea
manideep <17101992m@gmail.com>
2022-06-25 12:29:58 +0530

    Solved the merge sorted array using iteration method

--- Merge Sort.js
+++ Merge Sort.js
@@ -35,3 +35,26 @@ function mergeArray(arr1, arr2){
 }
 
 mergeArray([0,1,4,8,9], [2,3,5,23,34,97])
+
+// Solved using iteration, got hint from Colt Steele's solution.
+function mergeArray1(arr1, arr2){
+    let result1 = []
+    while(arr1.length > 0 && arr2.length > 0){
+        if(arr1[0] <= arr2[0]){
+            result1.push(arr1[0]);
+            arr1.splice(0, 1)
+        } else {
+            result1.push(arr2[0]);
+            arr2.splice(0, 1)
+        }
+    }
+    if(arr1.length === 0 && arr2.length > 0){
+        result1.push(...arr2)
+    }
+    else if(arr2.length === 0 && arr1.length > 0){
+        result1.push(...arr1)
+    }
+    console.log(result1)
+}
+
+mergeArray1([0,1,4,8,9], [2,3,5,23,34,97])
