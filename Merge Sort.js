451be3b8c88a32b08325bfb929b5fc3ed8e3df54
manideep <17101992m@gmail.com>
2022-06-25 11:26:07 +0530

    Merging array pseducode added

---
+++ Merge Sort.js
@@ -0,0 +1,11 @@
+/*         Merging array pseducode
+
+-> Create an empty array, take a look at the smallest values in each input array.
+
+-> While there are still values we haven't looked at
+    -> If the value in the first array is smallest than the value in the second array, push the value in the first array into our results and move on to the next value in the first array.
+
+    -> If the value in the first array is larger than the value in the second array, push the value in the second array into out result and move onto the next value in the second array.
+
+    -> Once we axhuast one array, push in all remaining value from the other array. */
+    
