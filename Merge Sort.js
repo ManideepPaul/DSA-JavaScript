a85570d6a3f368a4f0fca4600c3876cb89a33393
manideep <17101992m@gmail.com>
2022-06-25 12:07:00 +0530

    Merging two sorted array into one sorted array solved my myself

--- Merge Sort.js
+++ Merge Sort.js
@@ -8,4 +8,30 @@
     -> If the value in the first array is larger than the value in the second array, push the value in the second array into out result and move onto the next value in the second array.
 
     -> Once we axhuast one array, push in all remaining value from the other array. */
-    
+
+
+// This is my solution
+let result = [];
+function mergeArray(arr1, arr2){
+    if(arr1.length === 0){
+        result.push(...arr2)
+        console.log(result)
+        return result
+    }
+    else if(arr2.length === 0) {
+        result.push(...arr1)
+        console.log(result)
+        return result
+    }
+    if(arr1[0] < arr2[0]) {
+        result.push(arr1[0]);
+        arr1.splice(0, 1)
+    }
+    else {
+        result.push(arr2[0]);
+        arr2.splice(0, 1)
+    }
+    mergeArray(arr1, arr2)
+}
+
+mergeArray([0,1,4,8,9], [2,3,5,23,34,97])
