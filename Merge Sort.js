44b734c20c6369d5c5315edbded4369ee96b203c
manideep <17101992m@gmail.com>
2022-06-26 20:55:40 +0530

    breaking a array into half

--- Merge Sort.js
+++ Merge Sort.js
@@ -5,3 +5,13 @@
 ->  Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array.
 
 -> Once the array has been merged back together, return the merged (and sorted!) array. */
+
+let arrays = []
+function mergeSort(arr) {
+    let halfLength = (arr.length - 1) / 2;
+    arrays.push(arr.slice(0, Math.floor(halfLength)));
+    arrays.push(arr.slice(Math.floor(halfLength), arr.length));
+}
+
+mergeSort([1, 3, 2, 8, 67, 23, 34, 54, 12])
+console.log(arrays)
