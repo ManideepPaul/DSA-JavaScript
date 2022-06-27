1b1b58a00fde835bb486a394e149d1a34101de5e
manideep <17101992m@gmail.com>
2022-06-27 12:30:37 +0530

    Merge sort solved

--- Merge Sort.js
+++ Merge Sort.js
@@ -6,12 +6,33 @@
 
 -> Once the array has been merged back together, return the merged (and sorted!) array. */
 
-let arrays = []
 function mergeSort(arr) {
+    if(arr.length <= 1) return arr;
     let halfLength = Math.floor(arr.length / 2);
-    arrays.push(arr.slice(0, halfLength));
-    arrays.push(arr.slice(halfLength));
+    let left = mergeSort(arr.slice(0, halfLength));
+    let right = mergeSort(arr.slice(halfLength));
+    return mergeArray1(left , right);
 }
 
-mergeSort([1, 3, 2, 8, 67, 23, 34, 54, 12])
-console.log(arrays)
+console.log(mergeSort([1, 3, 2, 8, 67, 23, 34, 54, 12]))
+
+function mergeArray1(arr1, arr2){
+    let result1 = []
+    while(arr1.length > 0 && arr2.length > 0){
+        if(arr1[0] <= arr2[0]){
+            result1.push(arr1[0]);
+            arr1.splice(0, 1)
+        } else {
+            result1.push(arr2[0]);
+            arr2.splice(0, 1)
+        }
+    }
+    if(arr1.length === 0 && arr2.length > 0){
+        result1.push(...arr2)
+    }
+    else if(arr2.length === 0 && arr1.length > 0){
+        result1.push(...arr1)
+    }
+    return result1;
+}
+
