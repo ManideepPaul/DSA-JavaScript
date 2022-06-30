1d79785d50cc446a4daf83844809a61c34f2cd3f
manideep <17101992m@gmail.com>
2022-06-30 12:19:03 +0530

    Solved the pivot helper solution

--- Pivot.js
+++ Pivot.js
@@ -1,4 +1,4 @@
-                    Pivot Pseducode
+/*                     Pivot Pseducode
 
 -> It will help to accept three arguments: an Array, a start index, and end index. (these can be default to 0 and the array length minus 1, respectively)
 
@@ -12,4 +12,21 @@
 
 -> Swap the starting element( i.e the pivot) with the pivot index. 
 
--> Return the pivot index.
+-> Return the pivot index. */
+
+function pivot(arr){
+    let pivotIndex = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[0] > arr[i]){
+            if(i === 1){pivotIndex++;}
+            else{
+                pivotIndex++;
+                [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]]
+            } 
+        }
+    }
+    [arr[0], arr[pivotIndex]] = [arr[pivotIndex], arr[0]];
+    console.log(arr, pivotIndex)
+}
+
+pivot([5,2,1,3,6,4])
