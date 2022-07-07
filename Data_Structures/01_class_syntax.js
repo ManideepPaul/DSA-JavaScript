b6d8da9420a6d26480745a591ad61c42cf5563ca
manideep <17101992m@gmail.com>
2022-07-07 09:38:05 +0530

    Class method added

--- Data_Structures/01_class_syntax.js
+++ Data_Structures/01_class_syntax.js
@@ -28,6 +28,12 @@ class Student {
         let total = this.score.reduce((a,b) => a+b)
         console.log(Math.round(total/this.score.length))
     }
+
+    // This is class method and it is can't be called by individual instance.
+    //  -> To make it a class method we need to use "static" keyword before initating the method itself.
+    static calssMethod(){
+        console.log('I AM A CLASS METHOD')
+    }
 }
 
 /* Some Pointers for creating class
@@ -49,4 +55,15 @@ student1.addScore(91)
 student1.addScore(95)
 student1.addScore(98)
 student1.calcAverageScore()
+
+Student.calssMethod()// Calling class method on the class;
+
+/*  ******************** ALL POINTERS ***********************
+
+->  Classes are blueprints that when created make objects known as instances. 
+->  Class are created with the "new" keyword. 
+->  The constructor function is a special function that gets run when the class is instantiated
+->  Instance method can be added to classes similar to methods in objects. 
+->  Class methods can be added using the static keyword. 
+ */
 
