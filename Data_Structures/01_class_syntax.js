a1c21e44420c20c78190f92a8a3805501c20f8a9
manideep <17101992m@gmail.com>
2022-07-07 08:54:17 +0530

    Method adding syntax added in class

--- Data_Structures/01_class_syntax.js
+++ Data_Structures/01_class_syntax.js
@@ -8,6 +8,16 @@ class Student {
         this.first = firstName;
         this.last = lastName;
         this.age = age;
+        this.absence = 0;
+    }
+
+    // This is a method on individual instance.
+    absentDay(){
+        this.absence++
+        console.log(`Total number of absent day is ${this.absence}`)
+    }
+    fullName(){
+        console.log(`Your full name is ${this.first} ${this.last}`)
     }
 }
 
@@ -23,3 +33,7 @@ let student1 = new Student ("Manideep", "Paul", 29)
 console.log(student1.first, student1.last, student1.age)
 
 /* Note    ->  When "this" used inside a constructor then "this" refers to individual instances of that class. Ex - student1 is an individual instance. */
+
+student1.absentDay() // You can call methods on individual instance like this.
+student1.absentDay()
+student1.fullName()
