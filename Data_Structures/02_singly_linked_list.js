f36da6bd825214ccddfad01d166acc8749a91734
manideep <17101992m@gmail.com>
2022-07-08 09:11:11 +0530

    What is singly linked list

---
+++ Data_Structures/02_singly_linked_list.js
@@ -0,0 +1,6 @@
+/*                         Linked List 
+
+-> A data structure that contains a Headers, tail and length property. 
+-> Linked lists consists of NodeList, and each node has value and a pointer to another node or null. 
+ */
+
