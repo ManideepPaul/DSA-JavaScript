c5f6a712e583b9bb5268652df275990f98d3f4d4
manideep <17101992m@gmail.com>
2022-07-08 09:39:33 +0530

    Singly linked list structure created

--- Data_Structures/02_singly_linked_list.js
+++ Data_Structures/02_singly_linked_list.js
@@ -4,3 +4,20 @@
 -> Linked lists consists of NodeList, and each node has value and a pointer to another node or null. 
  */
 
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
+
+let newList = new SinglyLinkedList;
+console.log(newList)
