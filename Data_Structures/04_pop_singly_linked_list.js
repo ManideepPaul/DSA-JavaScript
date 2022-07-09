570595884911c97ff30bd2a8118def36b77f74db
manideep <17101992m@gmail.com>
2022-07-09 11:32:40 +0530

    pop Pseducode added

---
+++ Data_Structures/04_pop_singly_linked_list.js
@@ -0,0 +1,8 @@
+/*                     Popping Pseudocode
+
+->  If there are no nodes on the list, return undefined.
+->  Loop through the entire list until you reach the tail. 
+->  Set the next prioperty to be the 2nd to last node. 
+->  Set the tail to be the 2nd to last node. 
+->  Decrement the length of the list by 1.
+->  Return the value of the node removed. */
