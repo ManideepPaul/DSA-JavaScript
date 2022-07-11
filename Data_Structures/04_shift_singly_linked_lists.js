1d7f1d64127e023b6c66f97513d0923a3050dd15
manideep <17101992m@gmail.com>
2022-07-11 09:10:12 +0530

    Shifting pseducode addded

---
+++ Data_Structures/04_shift_singly_linked_lists.js
@@ -0,0 +1,8 @@
+/*                         Shifting Pseudocode
+                    
+->  If there are no nodes, return undefined. 
+->  Store the current head property in a variable. 
+->  Set the head property to be the current head's next property. 
+->  Decrement the length by one. 
+->  Return the value of the removed node. 
+ */
