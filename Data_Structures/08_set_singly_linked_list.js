50b6659cd36c55a9ec5226c519745fb06a9229b4
manideep <17101992m@gmail.com>
2022-07-13 09:13:15 +0530

    set pseudocode added

---
+++ Data_Structures/08_set_singly_linked_list.js
@@ -0,0 +1,7 @@
+/*                             Set Pseudocode
+
+-> This function should accep an index and a value
+-> Use your get function to find the specific node
+-> If the node is not found, return false
+-> If the node is found, set the value of the node to be the value passed to the function and return true
+ */
