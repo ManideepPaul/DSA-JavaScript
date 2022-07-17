d5b219347fe0bb2b7b9b9e7cf0b807eb67377ebc
manideep <17101992m@gmail.com>
2022-07-17 16:43:40 +0530

    remove pseudocode added

---
+++ Data_Structures/10_remove_singly_linked_list.js
@@ -0,0 +1,11 @@
+/*                             Remove Pseudocode
+
+-> If the index is less than zero, greateer than the length, return undefined. 
+-> If the index is the same as the length -1 pop 
+-> If the index is zero shift 
+-> Otherwise, using the get method, access the node st the index - 1.
+-> Set the next property on that node to be the next of the next node. 
+-> Decrement the length 
+-> Return the value of the node removed
+ */
+
