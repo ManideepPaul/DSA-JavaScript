9837d956b4a34e5ab6d7cc4c43cf13519117b109
manideep <17101992m@gmail.com>
2022-07-19 18:14:04 +0530

    Name rectified

---
+++ Data_Structures/01_doubly_linked_list.js
@@ -0,0 +1,17 @@
+                        // Setting Up Node Structure 
+                        
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(val) {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
