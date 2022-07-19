c3902526166854ad81b0c566bfb6229fac1c3953
manideep <17101992m@gmail.com>
2022-07-19 18:42:03 +0530

    Push method added to the doubly linked list

--- Data_Structures/02_doubly_linked_list.js
+++ Data_Structures/02_doubly_linked_list.js
@@ -8,3 +8,40 @@
 ->  Increment the length. 
 ->  Return the doubly linked list 
  */
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(val) {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+let newList = new DoublyLinkedList;
+newList.push(100)
+newList.push(101)
+newList.push("Last Item")
+console.log(newList)
