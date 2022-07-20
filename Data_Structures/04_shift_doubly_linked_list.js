6f636031d43120930ccc519f4f713794f71ce03e
manideep <17101992m@gmail.com>
2022-07-20 18:08:36 +0530

    Shift pseudocode added

---
+++ Data_Structures/04_shift_doubly_linked_list.js
@@ -0,0 +1,11 @@
+/*                         Shift Pseudocode
+
+->  If there is no head return undefined
+->  Store the current head in a variable
+->  If length is 1, set the head and tail to be null 
+->  Update the head to be the next node 
+->  Set the new head's previous to be the be null 
+->  set the old head's next to be the null 
+->  Decrement the length 
+->  Return the removed node 
+ */
