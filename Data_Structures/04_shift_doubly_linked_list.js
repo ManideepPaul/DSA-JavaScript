4535f6ee387f8db1340c1452a7076020fd51dbde
manideep <17101992m@gmail.com>
2022-07-20 18:27:54 +0530

    shift method added to the doubly linked list

--- Data_Structures/04_shift_doubly_linked_list.js
+++ Data_Structures/04_shift_doubly_linked_list.js
@@ -9,3 +9,73 @@
 ->  Decrement the length 
 ->  Return the removed node 
  */
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(val) {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if(this.length === 0) return undefined;
+        let removed = this.tail;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }
+        else {
+            this.tail = removed.prev;
+            this.tail.next = null;
+            removed.prev = null;
+        }
+        this.length--;
+        return removed;
+    }
+
+    shift() {
+        if(!this.head) return undefined;
+        let removedHead = this.head;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        }
+        else {
+            this.head = removedHead.next;
+            this.head.prev = null;
+            removedHead.next = null;
+        }
+        this.length--;
+        return removedHead;
+    }
+}
+
+let newList = new DoublyLinkedList;
+newList.push(100)
+newList.push(101)
+newList.push("Last Item")
+console.log(newList.shift())
+console.log(newList)
