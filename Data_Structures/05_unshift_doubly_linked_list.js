4386f473e6c904a27375be35027e0d4ebfa4e731
manideep <17101992m@gmail.com>
2022-07-21 11:51:36 +0530

    Unshift pseudocode added

---
+++ Data_Structures/05_unshift_doubly_linked_list.js
@@ -0,0 +1,10 @@
+/*                         Unshift Pseudocode
+
+->  create a new node with passed value
+->  if there is no head set the new node as head and tail 
+->  Set old head's prev to new node 
+->  set new node's next to old Node
+->  set new node as head 
+->  Increment the length 
+->  return list
+ */
