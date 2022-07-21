f84dc1cdca4f84d7f771bf72a27511f99bbbf051
manideep <17101992m@gmail.com>
2022-07-21 12:24:10 +0530

    Get pseudocode added

---
+++ Data_Structures/06_get_doubly_linked_list.js
@@ -0,0 +1,10 @@
+/*                             Get Pseudocode
+                    
+->  If the index is less than 0 or greater or equal to the length, return null 
+->  If index is less than or equal to the half of the length of the list 
+    ->  Loop through the list starting from the head and loop towards the middle
+    ->  Return the node once it is found 
+->  If the index is greater than the half of the length of the list. 
+    ->  Loop through the list starting from the tail and loop towards the middle
+    ->  Return the node once it is found
+ */
