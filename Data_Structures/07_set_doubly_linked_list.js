a02ce3038ec3d4a14b2d11691d77b1c83936d9a5
manideep <17101992m@gmail.com>
2022-07-25 09:03:12 +0530

    set pseudocode added

---
+++ Data_Structures/07_set_doubly_linked_list.js
@@ -0,0 +1,7 @@
+/*                                 Set Pseudocode
+
+->  Create a variable which is the result of the get method at the index passed to the function.
+->  If the get method return a valid node, set the value of that node to be the value passed to the function.
+->  Return true.
+ */
+
