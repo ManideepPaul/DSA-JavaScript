47806e95b32313519e8c3db5f9bb3ead65799f13
manideep <17101992m@gmail.com>
2022-07-25 11:11:28 +0530

    insert pseudocode added

---
+++ Data_Structures/08_insert_doubly_linked_list.js
@@ -0,0 +1,11 @@
+/*                         Insert Pseudocode
+
+->  If the index is less than 0 or greater than the length return false 
+->  If the index is 0 unshift
+->  If the index is same as the length push 
+->  Use get method to access the index - 1
+->  Set the next and the previous properties on the correct nodes to link everything together. 
+->  Increment the length 
+->  Return true 
+ */
+
